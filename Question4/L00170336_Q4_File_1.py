f15e79bed9579f319f7ce5594cebb155f338aa8f
L00170336 <L00170336@student.lyit.ie>
2021-12-01 16:08:46 +0000

    Added new word files and updated code for Q4

--- Question4/L00170336_Q4_File_1.py
+++ Question4/L00170336_Q4_File_1.py
@@ -5,30 +5,30 @@
 #	L00170336
 
 
-#
 """
 import socket
 import threading
+import sys
 
-target = "192.168.220.129"   # This is the name of my Ubunto server I want to connect to, to scan
+target = "192.168.220.129"  # Which Host to Scan
+
+
 def port_scanner(port):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        if port == 22:  # Return SSH if Port 22 open
-            print(f"SSH is open     ")
-        elif port == 25:  # Return SMTP if Port 25 open
-            print(f"SMTP is open     ")
-        elif port == 80:  # Return HTTP if Port 80 open
-            print(f"HTTP is open     ")
-        elif port == 3389:  # Return RDP if Port 3389 open
-            print(f"RDP is open     ")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        socket.getfqdn("8.8.8.8")
+
+        if port == 22:  # Will print SSH if this port is open
+            print(f"SSH is open")
+        elif port == 80:  # Will print HTTP if this port is open
+            print(f"HTTP is open")
         else:
-            print(f"{port} is open     ")
+            print(f"{port} is open")
     except:
         pass
 
 
-for port in range(1,5050):
-    thread = threading.Thread(target =port_scanner, args=[port])
+for port in range(1, 1025):
+    thread = threading.Thread(target=port_scanner, args=[port])
     thread.start()
